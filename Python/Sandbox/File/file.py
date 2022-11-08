b8b6bd66cd726690a60e1a4057e1fc5c9da7e224
CettaMa <cettamaulana96@gmial.com>
2022-11-08 10:34:01 +0700

    update on pra Uts

--- Python/Sandbox/File/file.py
+++ Python/Sandbox/File/file.py
@@ -1,5 +1,129 @@
 import json
 
+def simpandict(mode) :
+    if mode==1:
+        with open("Python/Sandbox/File/barang.json","w") as file1:
+            json.dump(dict1,file1,indent=4)
+    else :
+        with open("Python/Sandbox/File/jual.json","w") as file2:
+            json.dump(dict2,file2,indent=4)
+
+def isidict(mode):
+    if mode==2:
+        with open("Python/Sandbox/File/jual.json","r") as file2:
+            filejual=json.load(file2)
+        return filejual
+        
+    else :
+        with open("Python/Sandbox/File/barang.json","r") as file1:
+            filebarang=json.load(file1)
+        return filebarang
+
+def tambahbar(kode,nama,jenis,jumlah,hargabeli) :
+    dict1.update({7 : {"kode" : kode ,"nama" : nama,"jenis" : jenis,"jumlah" : jumlah,"Hargabeli" : hargabeli,"sisa" : 0,"untung" : 0}})
+
+def ubah(kode,nama,jenis) :
+    for i in dict1 :
+        if dict1[i]["kode"]==kode:
+            x=i
+
+    dict1[x]["nama"]=nama
+    dict1[x]["jenis"]=jenis
+
+def revisi() :
+    for x in dict1:
+        if dict1[x]["kode"]==500:
+            pass
+        else :
+            dict1[x]["aktif"]='y'
+    for x in dict1.items() :
+        print(x)
+
+def urut():
+    with open("Python/Sandbox/File/jual.json","r") as file2:
+        filejual=json.load(file2)
+
+    dict(filejual)
+
+    lap=(sorted(filejual.items(), key=lambda item: item[1]["kode"]))
+    filejual=dict(lap)
+
+    with open("Python/Sandbox/File/jual.json","w") as file2:
+        json.dump(filejual,file2,indent=4)
+
+def sisabarang():
+
+    filejual=isidict(2)
+
+    for barang1,keys1 in dict1.items():
+        dict1[barang1]["sisa"]= dict1[barang1]["jumlah"]
+        untung=0
+        for barang2,keys2 in filejual.items():
+            if keys1["kode"]==keys2["kode"] :
+                # print("ketemu!!",keys1["kode"])
+                sisa=keys1["sisa"]-keys2["jumlah"]
+                dict1[barang1]["sisa"]=sisa
+                # print(dict1[barang1]["sisa"])
+
+                if keys2["diskon"]==0 :
+                    untung +=(keys2["harga"]-keys1["Hargabeli"])*keys2["jumlah"]
+                else :
+                    untung +=((keys2["harga"]*(1-(keys2["diskon"]/100)))-keys1["Hargabeli"])*keys2["jumlah"]
+            else :
+                continue
+        dict1[barang1]["untung"]=untung
+
+def laporan() :
+    dict1=isidict(1)
+    print("Data Dagangan")
+    for keys,item in dict1.items() :
+        print(item["kode"],item["nama"],"\t",item["jenis"],"    \t",item["jumlah"],"\t",item["Hargabeli"],"\t",item["sisa"],"\t",item["untung"])
+    lap=(sorted(dict1.items(), key=lambda item: item[1]["jenis"]))
+    dict1=dict(lap)
+
+    print("\nSetelah diurutkan Berdasarkan Jenis :")
+    for keys,item in dict1.items() :
+        print(item["kode"],item["nama"],"\t",item["jenis"],"    \t",item["jumlah"],"\t",item["Hargabeli"],"\t",item["sisa"],"\t",item["untung"])
+
+    simpandict(1)
+
+    #menampilkan total untung tiap jenis
+    dict1=isidict(1)
+
+    untungpokok=0
+    untungsayuran=0
+    untungtambahan=0
+
+    for keys,item in dict1.items():
+        if item["jenis"]=="pokok":
+            untungpokok+=item["untung"]
+        if item["jenis"]=="sayuran":
+            untungsayuran+=item["untung"]
+        if item["jenis"]=="tambahan":
+            untungtambahan+=item["untung"]
+
+    print("\nlaporan Keuntungan dari tiap jenis\t:")
+    print("1\tPokok\t",untungpokok)
+    print("2\tSayuran\t",untungsayuran)
+    print("3\tPokok\t",untungtambahan)
+
+    with open("Python/Sandbox/File/laporan.txt","w") as file3:
+        file3.write("Laporan Keuntungan")
+        file3.write("\n")
+        file3.write("Pokok :")
+        file3.write(str(untungpokok))
+        file3.write("\n")
+        file3.write("Sayuran :")
+        file3.write(str(untungsayuran))
+        file3.write("\n")
+        file3.write("Tambahan :")
+        file3.write(str(untungtambahan))
+        file3.write("\n")
+        file3.write("Total Untung :")
+        file3.write(str(untungpokok+untungtambahan+untungsayuran))
+        file3.close
+
+
 dict1 = {
     1 : {"kode" : 100 ,"nama" : "bayam","jenis" : "sayuran","jumlah" : 15,"Hargabeli" : 1500,"sisa" : 0,"untung" :0},
     2 : {"kode" : 200 ,"nama" : "kangkung","jenis" : "sayuran","jumlah" : 10,"Hargabeli" : 1000,"sisa" : 0,"untung" : 0},
@@ -8,10 +132,7 @@ dict1 = {
     5 : {"kode" : 500 ,"nama" : "ubi","jenis" : "pokok","jumlah" : 15,"Hargabeli" : 2000,"sisa" : 0,"untung" : 0},
     6 : {"kode" : 600 ,"nama" : "wortel","jenis" : "sayuran","jumlah" : 20,"Hargabeli" : 4000,"sisa" : 0,"untung" : 0}
 }
-
-#isibarang 
-with open("Python/Sandbox/File/barang.json","w") as file1:
-    json.dump(dict1,file1,indent=4)
+simpandict(1)
 
 dict2 = {
     "barang1" : {"tanggal" : 101020,"kode" : 200,"jumlah" : 1,"harga" : 1200,"diskon" : 5},
@@ -26,103 +147,18 @@ dict2 = {
     "barang10" : {"tanggal" : 131020,"kode" : 300,"jumlah" : 1,"harga" : 15000,"diskon" : 10}
 }
 
-def tambahbar(kode,nama,jenis,jumlah,hargabeli) :
-    dict1.update({7 : {"kode" : kode ,"nama" : nama,"jenis" : jenis,"jumlah" : jumlah,"Hargabeli" : hargabeli,"sisa" : 0,"untung" : 0}})
+simpandict(2)
 
 tambahbar(700,"tempe","pokok",19,3119)
 
-def ubah(kode,nama,jenis) :
-    for i in dict1 :
-        if dict1[i]["kode"]==kode:
-            x=i
-
-    dict1[x]["nama"]=nama
-    dict1[x]["jenis"]=jenis
-
 ubah(700,"tempecetta","makanan")
 
-def revisi() :
-    for x in dict1:
-        if dict1[x]["kode"]==500:
-            pass
-        else :
-            dict1[x]["aktif"]='y'
-    for x in dict1.items() :
-        print(x)
-    
 revisi()
 
-#isijual
-with open("Python/Sandbox/File/jual.json","w") as file2:
-    json.dump(dict2,file2,indent=4)
+urut()
 
-#urut
-with open("Python/Sandbox/File/jual.json","r") as file2:
-    filejual=json.load(file2)
+sisabarang()
 
-dict(filejual)
+simpandict(1)
 
-lap=(sorted(filejual.items(), key=lambda item: item[1]["kode"]))
-filejual=dict(lap)
-
-with open("Python/Sandbox/File/jual.json","w") as file2:
-    json.dump(filejual,file2,indent=4)
-
-#nomor.D
-for barang1,keys1 in dict1.items():
-    dict1[barang1]["sisa"]= dict1[barang1]["jumlah"]
-    untung=0
-    for barang2,keys2 in filejual.items():
-        if keys1["kode"]==keys2["kode"] :
-            # print("ketemu!!",keys1["kode"])
-            sisa=keys1["sisa"]-keys2["jumlah"]
-            dict1[barang1]["sisa"]=sisa
-            # print(dict1[barang1]["sisa"])
-
-            if keys2["diskon"]==0 :
-                untung +=keys2["harga"]-keys1["Hargabeli"]
-            else :
-                untung +=(keys2["harga"]*(1-(keys2["diskon"]/100)))-keys1["Hargabeli"]
-        else :
-            continue
-    dict1[barang1]["untung"]=untung
-
-with open("Python/Sandbox/File/barang.json","w") as file1:
-    json.dump(dict1,file1,indent=4)
-
-#Menampilkan data dari file dan mengurutkan berdasarkan jenis
-with open("Python/Sandbox/File/barang.json","r") as file1:
-    dict1=json.load(file1)
-
-print("Data Dagangan")
-for keys,item in dict1.items() :
-    print(item["kode"],item["nama"],"\t",item["jenis"],"    \t",item["jumlah"],"\t",item["Hargabeli"],"\t",item["sisa"],"\t",item["untung"])
-lap=(sorted(dict1.items(), key=lambda item: item[1]["jenis"]))
-dict1=dict(lap)
-print("\nSetelah diurutkan Berdasarkan Jenis :")
-for keys,item in dict1.items() :
-    print(item["kode"],item["nama"],"\t",item["jenis"],"    \t",item["jumlah"],"\t",item["Hargabeli"],"\t",item["sisa"],"\t",item["untung"])
-
-with open("Python/Sandbox/File/barang.json","w") as file1:
-    json.dump(dict1,file1,indent=4)
-
-#menampilkan total untung tiap jenis
-with open("Python/Sandbox/File/barang.json","r") as file1:
-    dict1=json.load(file1)
-
-untungpokok=0
-untungsayuran=0
-untungtambahan=0
-
-for keys,item in dict1.items():
-    if item["jenis"]=="pokok":
-        untungpokok+=item["untung"]
-    if item["jenis"]=="sayuran":
-        untungsayuran+=item["untung"]
-    if item["jenis"]=="tambahan":
-        untungtambahan+=item["untung"]
-
-print("\nlaporan Keuntungan dari tiap jenis\t:")
-print("1\tPokok\t",untungpokok)
-print("2\tSayuran\t",untungsayuran)
-print("3\tPokok\t",untungtambahan)
+laporan()
---
+++ Python/Sandbox/Prauts/prauts.py
@@ -0,0 +1,151 @@
+import json
+
+mahasiswa= {
+    1 : {"nama": "Budi Banjaran", "nim": 110401, "ipk": 0, "SKSLulus": 0},
+    2 : {"nama": "Cici Cianjur", "nim": 110402, "ipk": 0, "SKSLulus": 0},
+    3 : {"nama": "Bandung Bondowoso", "nim": 110403, "ipk": 0, "SKSLulus": 0},
+    4 : {"nama": "Edi Dermayu", "nim": 110404, "ipk": 0, "SKSLulus": 0},
+    5 : {"nama": "Dudi Durudi", "nim": 110405, "ipk": 0, "SKSLulus": 0},
+}
+
+nil = {
+    1 : {"nim" : 110401, "namamk" : "fisika", "sks" : 3, "uts" : 80, "uas" : 85, "indeks" : "x"},
+    2 : {"nim" : 110402, "namamk" : "fisika", "sks" : 3, "uts" : 70, "uas" : 60, "indeks" : "x"},
+    3 : {"nim" : 110403, "namamk" : "fisika", "sks" : 3, "uts" : 40, "uas" : 35, "indeks" : "x"},
+    4 : {"nim" : 110403, "namamk" : "kalkulus", "sks" : 4, "uts" : 30, "uas" : 30, "indeks" : "x"},
+    5 : {"nim" : 110404, "namamk" : "kalkulus", "sks" : 4, "uts" : 60, "uas" : 65, "indeks" : "x"},
+    6 : {"nim" : 110405, "namamk" : "kalkulus", "sks" : 4, "uts" : 90, "uas" : 70, "indeks" : "x"},
+    7 : {"nim" : 110401, "namamk" : "agama", "sks" : 2, "uts" : 90, "uas" : 80, "indeks" : "x"},
+    8 : {"nim" : 110402, "namamk" : "agama", "sks" : 2, "uts" : 75, "uas" : 60, "indeks" : "x"},
+    9 : {"nim" : 110401, "namamk" : "binggris", "sks" : 3, "uts" : 80, "uas" : 85, "indeks" : "x"},
+    10 : {"nim" : 110402, "namamk" : "binggris", "sks" : 3, "uts" : 70, "uas" : 70, "indeks" : "x"},
+    11 : {"nim" : 110403, "namamk" : "binggris", "sks" : 3, "uts" : 90, "uas" : 80, "indeks" : "x"},
+    12 : {"nim" : 110404, "namamk" : "binggris", "sks" : 3, "uts" : 30, "uas" : 40, "indeks" : "x"},
+    13 : {"nim" : 110405, "namamk" : "binggris", "sks" : 3, "uts" : 60, "uas" : 50, "indeks" : "x"},
+}
+
+def simpandict(mode):
+    if mode==1:
+        with open("Python/Sandbox/Prauts/datamhs.json","w") as filemhs:
+            json.dump(mahasiswa,filemhs)
+    else :
+        with open("Python/Sandbox/Prauts/datanilai.json","w") as filenilai:
+            json.dump(nil,filenilai)
+
+def loaddict(mode):
+    if mode==1:
+        with open("Python/Sandbox/Prauts/datamhs.json","r") as filemhs:
+            mahasiswa=json.load(filemhs)
+            dict(mahasiswa)
+    else :
+        with open("Python/Sandbox/Prauts/datanilai.json","r") as filenilai:
+            nil=json.load(filenilai)
+            dict(nil)
+
+def buatindeks():
+    loaddict(2)
+    print("\nMenghitung Indeks\t:")
+    for x in nil:
+        nilaiakhir=float((nil[x]["uts"]*4/10)+(nil[x]["uas"]*6/10))
+        if nilaiakhir>=80:
+            nil[x]["indeks"]="A"
+        elif nilaiakhir>=70 and nilaiakhir<80:
+            nil[x]["indeks"]="B"
+        elif nilaiakhir>=55 and nilaiakhir<70:
+            nil[x]["indeks"]="C"
+        elif nilaiakhir>=40 and nilaiakhir<55:
+            nil[x]["indeks"]="D"
+        elif nilaiakhir<40:
+            nil[x]["indeks"]="E"
+    tampilkan(2)
+    simpandict(2)
+
+def hitungip():
+    print("\nMenghitung IPK\t:")
+    loaddict(1)
+    loaddict(2)
+
+    for keys1,isi1 in mahasiswa.items():
+        jmlbobot=0
+        jmlsks=0
+        skslulus=0
+        for keys2,isi2 in nil.items():
+            if isi1["nim"]==isi2["nim"]:
+                jmlsks+=isi2["sks"]
+                if isi2["indeks"]=="A":
+                    jmlbobot+=4*isi2["sks"]
+                    skslulus+=1
+                elif isi2["indeks"]=="B":
+                    jmlbobot+=3*isi2["sks"]
+                    skslulus+=1
+                elif isi2["indeks"]=="C":
+                    jmlbobot+=2*isi2["sks"]
+                    skslulus+=1
+                elif isi2["indeks"]=="D":
+                    jmlbobot+=2*isi2["sks"]
+                elif isi2["indeks"]=="E":
+                    jmlbobot+=1*isi2["sks"]
+            else :
+                continue
+        ipk=jmlbobot/jmlsks
+        mahasiswa[keys1]["ipk"]=ipk
+        mahasiswa[keys1]["SKSLulus"]=skslulus
+    tampilkan(1)
+    simpandict(1)
+
+def urut(mode):
+    if mode==1:
+        file= open("Python/Sandbox/Prauts/datanilai.json","r")
+        filenilai=json.load(file)
+        lap=(sorted(filenilai.items(),key=lambda item: item[1]["nim"]))
+        nil=dict(lap)
+        print("\nMengurutkan Berdasarkan NIM (Acending)")
+        tampilkan(2)
+        simpandict(2)
+
+    elif mode==2:
+        file= open("Python/Sandbox/Prauts/datanilai.json","r")
+        filenilai=json.load(file)
+        lap=(sorted(filenilai.items(),reverse=True, key=lambda item: item[1]["nim"]))
+        nil=dict(lap)
+        print("\nMengurutkan Berdasarkan NIM (Decending)")
+        tampilkan(2)
+        simpandict(2)
+
+    elif mode==3:
+        file= open("Python/Sandbox/Prauts/datamhs.json","r")
+        filemhs=json.load(file)
+        lap=(sorted(filemhs.items(), key=lambda item: item[1]["ipk"]))
+        mahasiswa=dict(lap)
+        print("\nMengurutkan Berdasarkan IPK")
+        tampilkan(1)
+        simpandict(1)
+
+    elif mode==4:
+        file= open("Python/Sandbox/Prauts/datamhs.json","r")
+        filemhs=json.load(file)
+        lap=(sorted(filemhs.items(),reverse=True, key=lambda item: item[1]["SKSLulus"]))
+        mahasiswa=dict(lap)
+        print("\nMengurutkan Berdasarkan SKS Lulus")
+        tampilkan(1)
+        simpandict(1)
+
+def peringkat():
+    urut(3)
+    print("Peringkat Mahasiswa\t:")
+    for i in range(1,4):
+        print(i,"\t",mahasiswa[i]["nim"],"\t",mahasiswa[i]["nama"],"    \t","%.2f" %mahasiswa[i]["ipk"])
+
+def tampilkan(mode):
+    if mode == 1:
+        loaddict(1)
+        for x in mahasiswa:
+            print(mahasiswa[x]["nim"],"\t",mahasiswa[x]["nama"],"   \t","%.2f"%mahasiswa[x]["ipk"],"\t",mahasiswa[x]["SKSLulus"])
+    elif mode == 2:
+        loaddict(2)
+        for x in nil:
+            print(nil[x]["nim"],"\t",nil[x]["namamk"],"   \t",nil[x]["sks"],"\t",nil[x]["uts"],"\t",nil[x]["uas"],"\t",nil[x]["indeks"])
+
+buatindeks()
+hitungip()
+peringkat()
