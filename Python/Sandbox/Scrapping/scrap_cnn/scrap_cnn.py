9133c1e9bd1a64f32fe01d7b18c74de993d353a1
CettaMa <CettaMaulana96@gmail.com>
2022-12-28 18:35:17 +0700

    update on tree

---
+++ Python/Sandbox/Scrapping/scrap_cnn/scrap_cnn.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+pointer="//*[@id=\"content\"]/div/div[1]/div/div[1]/article["
+
+artikel={}
+
+def parser_para():
+
+    for page in range(1,6):
+        newest=driver.find_element(By.XPATH, pointer + str(page)+"]/a")
+        newest.click()
+        req=requests.get(driver.current_url)
+        soup= BeautifulSoup(req.content,'html.parser')
+        s = soup.find('div',class_='detail_text')
+        if s == None:
+            pass
+        else :
+            content = s.find_all('p')
+            artikel[page]={"judul": soup.title.text}
+            isi=""
+            for line in content:
+                isi+=line.text
+            artikel[page]={"isi": isi}
+     
+            with open("Python/Sandbox/Scrapping/scrap_cnn/scrap.txt","a+") as file:
+                file.write("\t")
+                file.write(soup.title.text)
+                file.write("\n")
+                for line in content:
+                    file.write(line.text)
+                    file.write("\n")
+                file.write("\n")
+        
+        driver.get(main_page_oto)
+    
+url="https://www.cnnindonesia.com/"
+
+driver=webdriver.Chrome(executable_path='./chromedriver') #menggunakan selenium untuk automasi
+driver.get(url)
+
+otomotif=driver.find_element(By.XPATH,'//*[@id="nav_menu"]/li[6]/a')
+otomotif.click()
+
+main_page_oto=driver.current_url
+
+parser_para()
+
+for line in artikel.items():
+    print(line)
---
+++ Python/Sandbox/Scrapping/scrap_kontan/scrapping_kontan.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+tanggal=input("Masukan tanggal : (Format : ##) :") #input berita tanggal berapa yang ingin discraping e.g 01
+bulan=input("Masukan bulan : (Format : ##) :") #e.g 12
+tahun=input("Masukan tahun : (Format : ####) :") #e.g 2022
+page=0
+
+#mencari jumlah page di dalam website
+nav=requests.get("https://www.kontan.co.id/search/indeks?kanal=investasi&tanggal="+str(tanggal)+"&bulan="+str(bulan)+"&tahun="+str(tahun)+"&pos=indeks&per_page="+str(page))
+isi_nav=bs(nav.content,"lxml")
+list=isi_nav.find('ul', class_="cd-pagination").text
+hal=int(list[-7]) #ini jumlah page didalam tanggal tersebut
+lnews=[]
+
+for n in range(hal):
+    url="https://www.kontan.co.id/search/indeks?kanal=investasi&tanggal="+str(tanggal)+"&bulan="+str(bulan)+"&tahun="+str(tahun)+"&pos=indeks&per_page="+str(page)
+    req=requests.get(url)
+    content=bs(req.content,'lxml')
+    list_berita=content.find('div', class_="list-berita")
+    for x in list_berita.find_all('a'):
+        if x.get('href')=="//investasi.kontan.co.id":
+            pass
+        else :
+            if "https:"+x.get('href') not in lnews:
+                lnews.append("https:"+x.get('href'))
+            else:
+                pass
+        
+    page+=20 #saat melihat url dari website, terlihat bahwa website menggunakan interval 20 pada setiap page nya, kenaikan ini digunkan website untuk berpindah page
+
+
+fl=open("Python/Sandbox/Scrapping/scrap_kontan/berita.txt","w").close()  
+for url in lnews:
+  cari = requests.get(url) #"https://investasi.kontan.co.id") #/communication-cable-systems-ccsi-akan-rights-issue-efek-dilusi-maksimal-1453")
+  bscari = bs(cari.content,'lxml')
+  #soup = bs(berita.content, features="lxml")
+  title = bscari.find("div",attrs={"class":"bag-kiri"})
+  judul = title.find("h1")
+  fl=open("berita.txt","a")
+  
+
+  print(judul.text+"***")  #*** digunakan untuk memisahkan judul dengan isi berita
+  fl.write(judul.text+"***")
+  
+
+  isi = bscari.find("div",attrs={"class":"tmpt-desk-kon"})
+  berita = isi.find_all("p")
+  #print("berita=",berita)
+  #input()
+
+  # Pengolahan text berita
+  for data in berita:
+    # Ada kalimat diawali "Baca Juga" dan "Cek Berita" untuk info tambahan di berita, yang harus dibuang
+    # kalimat tersebut ada di baris a tersendiri, jadi bisa dibuang hanya jika a mengandung :"Baca Juga".
+    if data.text.find("Baca Juga") >=0 or data.text.find("Cek Berita") >=0: pass
+    else: 
+      print(data.text)
+      fl.write(data.text)
+  fl.write("\n\n")
+  fl.close()
