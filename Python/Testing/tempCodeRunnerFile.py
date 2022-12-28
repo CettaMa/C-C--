29a259f9d42fb4a0004b134aa2723fa713060822
CettaMa <CettaMaulana96@gmail.com>
2022-12-28 18:35:42 +0700

    update on tree

--- Python/Testing/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-if line.find("div", class_="stream-usename") == None :
-    #     pass
-    # else:
-    #     username=line.find("div", class_="stream-username")
-    #     print(username.get_text())
