791c2d8a06f2db7a74b0da9068abe20e9af41606
CettaMa <93418128+CettaMa@users.noreply.github.com>
2023-12-07 20:01:17 +0700

    all

--- Python/Testing/test.py
+++ Python/Testing/test.py
@@ -1,39 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
+class mobil():
+    def __init__(self,nama,tahun,bahanBakar):
+        self.nama=nama
+        self.tahun=tahun
+        self.bahanBakar=bahanBakar
+        self._jRoda=4
 
-import time
-lnews=[]
-r=HTMLSession()
-#req=r.get("https://www.ipotnews.com/ipotnews/newsPages.php?level4=topnews#1")
-req=r.get("https://www.indopremier.com/ipotnews/nw-saham.php?level4=stocks#2")
-req.html.render(sleep=5)
-soup=BeautifulSoup(req.html.raw_html,"html.parser")
-container=soup.find("div",id="divMoreNewsPages")
-daftarlink=container.find_all('a')
-for x in daftarlink:
-    if "newsDetail" in x.get('href'):
-        lnews.append("https://www.ipotnews.com/ipotnews/"+x.get('href'))
+    def tampilInfo(self):
+        print("\nNama mobil :{}\nTahun :{}\nBahan Bakar :{}\n".format(self.nama,self.tahun,self.bahanBakar))
 
-for x in lnews:
-    print(x,"\n")
-    req=requests.get(x)
-    soup= BeautifulSoup(req.content,'lxml')
-    paragraft=soup.find('article')
-    fr= open("scrap.txt","r")
-    i=0
-    frdata=fr.read()
-    if soup.title.text in frdata: 
-      break
+mbim=mobil("corolla",2019,"Bensin")
+mbim.tampilInfo()
 
-    else:
-      with open("C:/Bayok/Irene/scrap.txt","a+") as file:
-        file.write("\t")
-        file.write(soup.title.text)
-        file.write(paragraft.text)
-        file.write("\n")
-        i=i+1
-    fr.close()
+class mobilBekas(mobil):
+    def __init__(self,nama,tahun,bahanBakar):
+        super().__init__(nama,tahun,bahanBakar)
+        self.__grade="\t"
+
+    @property
+    def grade(self):
+        pass
+    
+    @grade.setter
+    def grade(self, grade):
+        self.__grade=grade
+    
+    @grade.getter
+    def grade(self):
+        return self.__grade
+
+Mobil=mobilBekas("lexus",2003,"bensin")
+Mobil.grade= "tabrak"
+Mobil.tampilInfo()
+print("Grade : {}".format(Mobil.grade))
 
 
-print(len(lnews),i)
+print(Mobil._jRoda)
